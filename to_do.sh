8cf4d68339e46d26fc427665c946d1f1177260d7
gSchool Student <gSchool@Datas-Mac-mini-3.local>
2015-05-24 23:54:56 -0700

    update

--- to_do.sh
+++ to_do.sh
@@ -1,1 +1,7 @@
 brew install tmux
+
+./bin/spark-class org.apache.spark.deploy.worker.Worker  spark://127.0.0.1:7077
+
+IPYTHON_OPTS="notebook --ip=0.0.0.0" \
+ ${SPARK_HOME}/bin/pyspark \
+--master spark://127.0.0.1:7077
