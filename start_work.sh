595fdd9df5e5743f45674224a28bd41539451cf4
Jeffrey Tang <jeffrey.tang09@gmail.com>
2015-05-25 12:24:51 -0700

    Update start_work.sh

--- start_work.sh
+++ start_work.sh
@@ -1,1 +1,4 @@
-${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker  spark://127.0.0.1:7077
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker \
+--cores "$1" \
+--memory "$2" \
+spark://127.0.0.1:7077
