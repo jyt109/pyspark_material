8105c536c6cdd37536b84440893d02ea72f54b25
gSchool Student <gSchool@Datas-Mac-mini-3.local>
2015-05-25 01:49:39 -0700

    updated

---
+++ start_work.sh
@@ -0,0 +1,1 @@
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker  spark://127.0.0.1:7077
--- to_do.sh
+++ to_do.sh
@@ -1,7 +1,7 @@
 brew install tmux
 
+export SPARK_MASTER_IP=127.0.0.1
+$ ./sbin/start-master.sh
 ./bin/spark-class org.apache.spark.deploy.worker.Worker  spark://127.0.0.1:7077
 
-IPYTHON_OPTS="notebook --ip=0.0.0.0" \
- ${SPARK_HOME}/bin/pyspark \
---master spark://127.0.0.1:7077
+http://rustyrazorblade.com/2015/05/on-the-bleeding-edge-pyspark-dataframes-and-cassandra/
