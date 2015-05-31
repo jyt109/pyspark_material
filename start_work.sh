c3d30562368d004d6b98f9e0b31883ee29795575
jyt109 <jeffrey.tang09@gmail.com>
2015-05-30 18:52:50 -0700

    start_work updated

--- start_work.sh
+++ start_work.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Starting a Worker at local host at port 7077 with a specified amount of memory and cores
 
-${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker \
--c "$1" \
--m "$2" \
--h 127.0.0.1 \
--p 7077
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker spark://127.0.0.1:7077
