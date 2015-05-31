3b4c765666baffd63ca0691f819169bbe1c49451
jyt109 <jeffrey.tang09@gmail.com>
2015-05-30 18:48:39 -0700

    updated

--- start_master.sh
+++ start_master.sh
@@ -1,1 +1,7 @@
-${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master spark://127.0.0.1:7077
+#!/usr/bin/env bash
+# Starting a Master at local host at port 7077 and with UI at port 8080
+
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master \
+-h 127.0.0.1 \
+-p 7077 \
+--webui-port 8080
--- start_work.sh
+++ start_work.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
+# Starting a Worker at local host at port 7077 with a specified amount of memory and cores
+
 ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker \
---cores "$1" \
---memory "$2" \
-spark://127.0.0.1:7077
+-c "$1" \
+-m "$2" \
+-h 127.0.0.1 \
+-p 7077
